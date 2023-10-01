7f951866566c925341b4b610dc40e65fb824d01d
jy95 <jacques.yakoub@gmail.com>
2023-10-01 12:52:52 +0200

    chore: update story init

--- projects/ngx-json-schema-viewer/src/stories/Viewer.stories.ts
+++ projects/ngx-json-schema-viewer/src/stories/Viewer.stories.ts
@@ -1,11 +1,22 @@
 //import { moduleMetadata } from '@storybook/angular';
 
 import { NgxJsonSchemaViewerComponent } from "ngx-json-schema-viewer";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { moduleMetadata } from '@storybook/angular';
 
 import type { Meta, StoryObj } from '@storybook/angular';
 
 const meta : Meta<NgxJsonSchemaViewerComponent> = {
-    component: NgxJsonSchemaViewerComponent
+    //component: NgxJsonSchemaViewerComponent,
+    title: "Array",
+    decorators: [
+      moduleMetadata({
+        imports: [
+          BrowserAnimationsModule,
+          NgxJsonSchemaViewerComponent
+        ]
+      })
+    ]
 };
 
 export default meta;
@@ -14,6 +25,12 @@ type Story = StoryObj<NgxJsonSchemaViewerComponent>;
 
 // https://storybook.js.org/docs/angular/writing-stories/naming-components-and-hierarchy#grouping
 export const AdditionalItems1 : Story = {
+    render: (args) => ({
+      props: args,
+      template: `
+        <ngx-json-schema-viewer></ngx-json-schema-viewer>
+      `
+    }),
     args: {
         schema: {
             "type": "array",
