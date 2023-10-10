64918058913902482cd1348446460af3564cc4ef
jy95 <jacques.yakoub@gmail.com>
2023-10-11 00:15:06 +0200

    chore: refactor friendly-name (WIP)

--- projects/ngx-json-schema-viewer/src/lib/utils/FriendlyNames/friendly-name.ts
+++ projects/ngx-json-schema-viewer/src/lib/utils/FriendlyNames/friendly-name.ts
@@ -20,6 +20,8 @@ import {
 
 import type { JSONSchema, TypeValues } from "../../types";
 
+type renderTemplates = "booleanSchema" | "schemaTitle" | "schemaTypes" | "schemaFallback";
+
 @Component({
   selector: 'jsv-friendly-name',
   standalone: true,
@@ -31,34 +33,52 @@ import type { JSONSchema, TypeValues } from "../../types";
     forwardRef( () => GenerateFriendlyNameCustomComponent)
   ],
   template: `
-    <!--  In case the schema is always valid or not -->
-    <ng-container *ngIf="isBoolean" else titleOrCustomBlock>
-      <jsv-type-label-switch [type]="typedBoolean" />
-    </ng-container>
+    <!-- Final rendered friendly name -->
+    <ng-container [ngSwitch]="selectTemplate">
 
-    <ng-template #titleOrCustomBlock>
-
-      <!-- Some people maintaining schemas provide a friendly name by themself -->
-      <ng-container *ngIf="typedSchema.title; else customTypeBlock">
-        {{ typedSchema.title }}
+      <ng-container *ngSwitchCase="'booleanSchema'">
+        <ng-container *ngTemplateOutlet="booleanSchema"></ng-container>
       </ng-container>
 
-      <!-- Default strategy -->
-      <ng-template #customTypeBlock>
+      <ng-container *ngSwitchCase="'schemaTitle'">
+        <ng-container *ngTemplateOutlet="schemaTitle"></ng-container>
+      </ng-container>
 
-        <ng-container *ngIf="foundTypes.length === 0; else customTypeLoopBlock">
-          <jsv-friendly-name-fallback [schema]="typedSchema" />
+      <ng-container *ngSwitchCase="'schemaTypes'">
+        <ng-container *ngTemplateOutlet="schemaTypes"></ng-container>
+      </ng-container>
+
+      <ng-container *ngSwitchDefault>
+        <ng-container *ngTemplateOutlet="schemaFallback"></ng-container>
+      </ng-container>
+
+    </ng-container>
+
+    <!-- Templates -->
+
+    <!--  In case the schema is always valid or not -->
+    <ng-template #booleanSchema>
+      <jsv-type-label-switch [type]="typedBoolean" />
+    </ng-template>
+
+    <!-- Some people maintaining schemas provide a friendly name by themself -->
+    <ng-template #schemaTitle>
+      {{ typedSchema.title! }}
+    </ng-template>
+
+    <!-- Default strategy, when types are known -->
+    <ng-template #schemaTypes>
+      <ng-container *ngFor="let type of foundTypes; let isLast = last">
+        <jsv-friendly-name-custom [schema]="typedSchema" [type]="type"/>
+        <ng-container *ngIf="!isLast">
+          <labels-or />
         </ng-container>
+      </ng-container>
+    </ng-template>
 
-        <ng-template #customTypeLoopBlock>
-          <ng-container *ngFor="let type of foundTypes; let isLast = last">
-            <jsv-friendly-name-custom [schema]="typedSchema" [type]="type"/>
-            <ng-container *ngIf="!isLast">
-              <labels-or />
-            </ng-container>
-          </ng-container>
-        </ng-template>
-
+    <!-- Fallback, when none of the previous rules matched -->
+    <ng-template #schemaFallback>
+      <jsv-friendly-name-fallback [schema]="typedSchema" />
     </ng-template>
   `,
 })
@@ -79,6 +99,25 @@ export class GenerateFriendlyNameComponent {
 
   get typedSchema(): Exclude<JSONSchema, true | false> {
     return this.schema as Exclude<JSONSchema, true | false>;
+  }
+
+  // To know which template to render
+  get selectTemplate(): renderTemplates {
+    if (this.isBoolean) {
+      return "booleanSchema";
+    } else {
+      let castSchema = this.typedSchema;
+      
+      if (castSchema.title !== undefined) {
+        return "schemaTitle";
+      }
+
+      if (this.foundTypes.length !== 0) {
+        return "schemaTypes";
+      }
+
+      return "schemaFallback";
+    }
   }
 
 }
