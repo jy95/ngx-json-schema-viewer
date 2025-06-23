28f1bf72294df1e9ede4edbba146af88e0d25c3f
jy95 <9306961+jy95@users.noreply.github.com>
2025-06-23 21:57:05 +0000

    [create-pull-request] automated change

--- projects/ngx-json-schema-viewer/.storybook/main.ts
+++ projects/ngx-json-schema-viewer/.storybook/main.ts
@@ -2,35 +2,29 @@ import type { StorybookConfig } from "@storybook/angular";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    {
-      name: '@storybook/addon-styling-webpack',
-      options: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            implementation: require.resolve('sass'),
-                            sassOptions: {
-                                includePaths: ['node_modules'],
-                            },
-                        },
-                    },
-                ],
-            },
-        ],
-      },
+  addons: ["@storybook/addon-links", {
+    name: '@storybook/addon-styling-webpack',
+    options: {
+      rules: [
+          {
+              test: /\.scss$/,
+              use: [
+                  'style-loader',
+                  'css-loader',
+                  {
+                      loader: 'sass-loader',
+                      options: {
+                          implementation: require.resolve('sass'),
+                          sassOptions: {
+                              includePaths: ['node_modules'],
+                          },
+                      },
+                  },
+              ],
+          },
+      ],
     },
-    "@chromatic-com/storybook"
-  ],
+  }, "@chromatic-com/storybook", "@storybook/addon-docs"],
   framework: {
     name: "@storybook/angular",
     options: {},
