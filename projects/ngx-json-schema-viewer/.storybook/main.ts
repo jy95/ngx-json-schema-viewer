062db59d04f4b00a000a5549b5479841a243ccd2
Jacques Yakoub <jacques.yakoub@gmail.com>
2026-03-16 18:42:26 +0100

    Migrate main.ts to ESM format
    Migrated main.ts to valid ESM format with necessary imports.

--- projects/ngx-json-schema-viewer/.storybook/main.ts
+++ projects/ngx-json-schema-viewer/.storybook/main.ts
@@ -1,8 +1,14 @@
 // This file has been automatically migrated to valid ESM format by Storybook.
-import { createRequire } from "node:module";
-import type { StorybookConfig } from "@storybook/angular";
+import { createRequire } from 'node:module';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const require = createRequire(import.meta.url);
+
+// Types
+import type { StorybookConfig } from "@storybook/angular";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
