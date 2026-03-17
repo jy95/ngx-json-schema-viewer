44a1df521730506e17335cded40dc8a490757a45
Jacques Yakoub <jacques.yakoub@gmail.com>
2026-03-17 21:55:21 +0100

    Update preview.ts

--- projects/ngx-json-schema-viewer/.storybook/preview.ts
+++ projects/ngx-json-schema-viewer/.storybook/preview.ts
@@ -1,12 +1,9 @@
 import type { Preview } from "@storybook/angular";
-import { setCompodocJson } from "@storybook/addon-docs/angular";
-import { componentWrapperDecorator } from '@storybook/angular';
-import { themes } from 'storybook/theming';
-import docJson from "../documentation.json";
-setCompodocJson(docJson);
+import { componentWrapperDecorator } from "@storybook/angular";
+import { themes } from "storybook/theming";
 
 // Because of https://github.com/storybookjs/storybook/issues/30691
-import 'zone.js';
+import "zone.js";
 
 const preview: Preview = {
   parameters: {
@@ -20,13 +17,11 @@ const preview: Preview = {
       theme: themes.dark,
     }
   },
-
   decorators: [
     // https://material.angular.io/guide/theming#application-background-color
     componentWrapperDecorator((story) => `<div class="mat-app-background">${story}</div>`),
   ],
-
-  tags: ["autodocs"]
+  tags: ["autodocs"],
 };
 
 export default preview;
