5ab7f8764e156854183f33b15751f3c74f3a5873
Jacques Yakoub <jacques.yakoub@gmail.com>
2026-03-17 21:54:22 +0100

    Update main.ts

--- projects/ngx-json-schema-viewer/.storybook/main.ts
+++ projects/ngx-json-schema-viewer/.storybook/main.ts
@@ -1,37 +1,17 @@
-import { fileURLToPath } from 'node:url';
-
-// Types
 import type { StorybookConfig } from "@storybook/angular";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-links", {
-    name: '@storybook/addon-styling-webpack',
-    options: {
-      rules: [
-          {
-              test: /\.scss$/,
-              use: [
-                  'style-loader',
-                  'css-loader',
-                  {
-                      loader: 'sass-loader',
-                      options: {
-                          implementation: fileURLToPath(import.meta.resolve('sass')),
-                          sassOptions: {
-                              includePaths: ['node_modules'],
-                          },
-                      },
-                  },
-              ],
-          },
-      ],
-    },
-  }, "@chromatic-com/storybook", "@storybook/addon-docs"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-docs",
+    "@chromatic-com/storybook"
+  ],
   framework: {
     name: "@storybook/angular",
     options: {},
   },
   docs: {}
 };
+
 export default config;
